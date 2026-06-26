7c7b7f2df7486ace775e2bab1b2f89b6f30d5ab8
NOT-Aayush <aayush4373pandey@gmail.com>
2026-06-26 18:20:15 +0530

    repo updates

--- backend/models/userModel.js
+++
@@ -1,6 +0,0 @@
-import pool from "../config/db.js";
-
-export const getFaceService = async () =>{
-    const result = await pool.query("SELECT * FROM persons");
-    return result.rows;
-}
--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getFace } from "../controller/userController.js";
-const uRouter = express.Router();
-
-uRouter.post("/getface",getFace);
-
-export default uRouter;
